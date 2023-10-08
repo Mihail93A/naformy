f41402fd2ae3b39cb5057df03c519bb74f927725
Mihail93A <strannodan@mail.ru>
2023-10-08 20:57:17 +0500

    2

--- src/test/java/forma/forma1.java
+++ src/test/java/forma/forma1.java
@@ -1,55 +1,57 @@
 package forma;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.logevents.SelenideLogger.step;
 
 public class forma1 {
     @Test
     void successfulSearchTest() {
-        Configuration.pageLoadStrategy = "eager";
-        open("https://demoqa.com/automation-practice-form");
-        String userName = "Alex";
-        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
-        executeJavaScript("$('#fixedban').remove()");
-        executeJavaScript("$('footer').remove()");
-
+        step("Open registrations form",() -> {
+            //Configuration.pageLoadStrategy = "eager";//
+            Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";open("https://demoqa.com/automation-practice-form");
+            String userName = "Alex";
+            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+            executeJavaScript("$('#fixedban').remove()");
+            executeJavaScript("$('footer').remove()");
+            });
+        step("Fill form",() -> {
+            String userName = "Alex";
         $("#firstName").setValue(userName);
         $("#lastName").setValue("Egorov");
         $("#userEmail").setValue("alex@egorov.com");
-//        $("#gender-radio-1").click(); // wrong
-//        $("#gender-radio-3").parent().click(); // good
-//        $(byText("Other")).click(); // not very good
-        $("#genterWrapper").$(byText("Other")).click(); // best
-//        $("label[for=gender-radio-3]").click(); // good
+        $("#genterWrapper").$(byText("Other")).click();
         $("#userNumber").setValue("1234567890");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("July");
-//        $(".react-datepicker__month-select").selectOptionByValue("6");
         $(".react-datepicker__year-select").selectOption("2008");
-// <div class="react-datepicker__day--030 react-datepicker__day--outside-month">30</div>
-// <div class="react-datepicker__day--030"                                     >30</div>
         $(".react-datepicker__day--030:not(.react-datepicker__day--outside-month)").click();
-//        $x("//*[@class='react-datepicker__day--030'][not(contains(@class, 'react-datepicker__day--outside-month'))]").click();
         $("#subjectsInput").setValue("Math").pressEnter();
         $("#hobbiesWrapper").$(byText("Sports")).click();
         $("#uploadPicture").uploadFromClasspath("img/1.png");
-//        $("#uploadPicture").uploadFile(new File("src/test/resources/img/1.png"));
         $("#currentAddress").setValue("Some address 1");
         $("#state").click();
         $("#stateCity-wrapper").$(byText("NCR")).click();
-//        $("#react-select-3-option-0").click();
         $("#city").click();
         $("#stateCity-wrapper").$(byText("Delhi")).click();
         $("#submit").click();
-
+        });
+        step("Check form results",() -> {
+            String userName = "Alex";
         $(".modal-dialog").should(appear);
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
         $(".table-responsive").shouldHave(text(userName), text("Egorov"),
                 text("alex@egorov.com"), text("1234567890"));
+        });
     }
 }
