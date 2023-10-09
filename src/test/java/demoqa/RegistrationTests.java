7fc08d26c5c04e31ecba5f2423e8accf0a7b6868
Mihail93A <strannodan@mail.ru>
2023-10-09 14:34:29 +0500

    plus

--- src/test/java/demoqa/RegistrationTests.java
+++ src/test/java/demoqa/RegistrationTests.java
@@ -1,12 +1,9 @@
-package forma;
+package demoqa;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
-import com.codeborne.selenide.logevents.SelenideLogger;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
@@ -14,20 +11,20 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.logevents.SelenideLogger.step;
 
-public class forma1 {
+
+public class RegistrationTests extends TestBase {
+
     @Test
-    void successfulSearchTest() {
-        step("Open registrations form",() -> {
-            //Configuration.pageLoadStrategy = "eager";//
-            Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";open("https://demoqa.com/automation-practice-form");
-            String userName = "Alex";
+    void successfulRegistrationTest() {
+
+step("Open registration form", () -> {
+            open("https://demoqa.com/automation-practice-form");
             $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
             executeJavaScript("$('#fixedban').remove()");
             executeJavaScript("$('footer').remove()");
-            });
-        step("Fill form",() -> {
-            String userName = "Alex";
-        $("#firstName").setValue(userName);
+        });
+step("Fill form", () -> {
+        $("#firstName").setValue("Alex");
         $("#lastName").setValue("Egorov");
         $("#userEmail").setValue("alex@egorov.com");
         $("#genterWrapper").$(byText("Other")).click();
@@ -46,11 +43,10 @@ public class forma1 {
         $("#stateCity-wrapper").$(byText("Delhi")).click();
         $("#submit").click();
         });
-        step("Check form results",() -> {
-            String userName = "Alex";
+step("Check form results", () -> {
         $(".modal-dialog").should(appear);
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
-        $(".table-responsive").shouldHave(text(userName), text("Egorov"),
+        $(".table-responsive").shouldHave(text("Alex"), text("Egorov"),
                 text("alex@egorov.com"), text("1234567890"));
         });
     }
